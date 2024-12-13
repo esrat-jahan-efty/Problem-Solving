62fd7e3a90ddbdfcd9441c71779a72c4214a8cb8
Esrat Jahan Efty <jahanefty12345@gmail.com>
2024-12-13 20:15:04 +0600

    comit

---
+++ super_shop_bill.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    double total_bill=0;
+    scanf("%d",&n);
+    while(n--)
+    {
+        char item_type;
+        scanf(" %c",&item_type);
+        int price,quantity;
+        scanf("%d%d",&price,&quantity);
+
+        double pq=price*quantity;
+        double result;
+
+        if(item_type=='F')
+        {
+            result=((pq)+(pq*0.05)+(pq*0.1));
+        }
+        else if(item_type=='O')
+        {
+            result=((pq)+(pq*0.05)+(pq*0.075));
+        }
+        else
+        {
+            result=((pq)+(pq*0.05));
+        }
+
+        total_bill=total_bill+result;
+    }
+    printf("%.2lf\n",total_bill);
+    return 0;
+}
