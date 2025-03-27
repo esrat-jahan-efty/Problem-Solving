4cc13c37ff9fae892d720bfa740478d7d058f996
Esrat Jahan Efty <jahanefty12345@gmail.com>
2025-03-27 14:23:13 +0600

    27_MAR_2025

---
+++ gravity_flip.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[i])
+            {
+                int var=arr[i];
+                arr[i]=arr[j];
+                arr[j]=var;
+            }
+        }
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+    return 0;
+    
+
+
+}
---
+++ pangram.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    char s[n+1],alp[27]="abcdefghijklmnopqrstuvwxyz";
+    scanf("%s",s);
+    getchar();
+
+    int i=0;
+
+    while(s[i]!='\0')
+    {
+        if(s[i]>='A' && s[i]<='Z')
+        {
+            s[i]=s[i]+32;
+        }
+        i++;
+    }
+
+    int j=0,count=0;
+    while(alp[j]!='\0')
+    {
+        int k=0;
+        while(s[k]!='\0')
+        {
+            if(s[k]==alp[j])
+            {
+                count++;
+                break;
+            }
+            k++;
+        }
+        j++;
+    }
+
+    if(count==26)
+    {
+        printf("YES\n");
+    }
+    else{
+        printf("NO\n");
+    }
+    return 0;
+    
+}
